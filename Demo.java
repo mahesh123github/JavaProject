fb48db083cce6b48e3fad3402d920dc90f53f963
mahesh123github <maheshbajare2000@gmail.com>
2024-12-09 22:27:57 +0530

    first push

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.java.project;
+
+public class Demo {
+public static void main(String args[])
+{
+	int a =10;
+	int b = 20;
+	int c = a +b;
+	System.out.println("sum of two number "+c);
+}
+}
